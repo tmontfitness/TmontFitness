de2c5d88153f6e210ca866fc952b74ba48b2e959
thefitdevproject <thefitdevproject@gmail.com>
2020-12-07 20:53:04 -0500

    Initial Check in of TmontFitness code

---
+++ TmontFitness/src/main/java/com/tmontfitness/TmontFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.tmontfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmontFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmontFitnessApplication.class, args);
+	}
+
+}
